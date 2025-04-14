5ebf936cb3f91e2b0a78ae7630b0426e87d0b844
Ebubekir Tosun <tosunbekir91@gmail.com>
2025-04-14 12:32:19 +0200

    withdrawTest

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        BankAccount bankAccount = new BankAccount();
+        BankAccount ortakHesap = new BankAccount();
 
         Thread t1 = new Thread(new WithdrawTask(ortakHesap), "Thread-1");
+        Thread t2 = new Thread(new WithdrawTask(ortakHesap), "Thread-2");
+
+        t1.start();
+        t2.start();
     }
 }
