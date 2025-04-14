14606edb64f034298a34e87ea28beffa84951483
Ebubekir Tosun <tosunbekir91@gmail.com>
2025-04-14 22:34:13 +0200

    withdrawTest

--- src/WithdrawTask.java
+++ src/WithdrawTask.java
@@ -8,7 +8,7 @@ public class WithdrawTask implements Runnable {
     @Override
     public void run() {
         for (int i = 0; i < 10; i++) {
-            bankAccount.withdraw(40);
+            bankAccount.withdraw(140);
         }
     }
 }
